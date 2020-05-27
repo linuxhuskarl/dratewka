8b4278d3d755200953e033cece5740b48e9c19a2
unknown <grafiks11@gmail.com>
2020-05-27 17:54:01 +0200

    added even more info about MQSensor

--- 01-ESP32_firmware/include/MQSensor.hpp
+++ 01-ESP32_firmware/include/MQSensor.hpp
@@ -6,27 +6,27 @@
 /**
  * MQ-7 and MQ-135 sensors driver class.
  */
-class MQSensor
+class MQSensor ///< MQSensor class \date $Date: 2020/05/20 14:16:20 $
 {
 private:
-    uint8_t analogPin;
-    uint8_t enablePin;
+    uint8_t analogPin;  
+    uint8_t enablePin; 
 public:
     
     /**
-     * Constructor
-     * @param analogPin
+     * @brief Constructor
+     * @param analogPin 
      * @param enablePin
      */
     MQSensor(uint8_t analogPin, uint8_t enablePin);
     
     /**
-     * Destructor
+     * Destructor 
      */
     ~MQSensor();
 
     /**
-     * Function enables MQSensor for a specific period of time
+     * @brief Function enables MQSensor for a specific period of time
      * @param time - parameter desribing time of enabling MQSensor
      */
     void enable(int time);
@@ -37,7 +37,7 @@ public:
     void disable();
     
     /**
-     * getValue function
+     * @brief getValue function
      * Function returning float as a reasult of operation getValue.
      * @param a - first argument 
      * @param b - second argument 
